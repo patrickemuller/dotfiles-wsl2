bc6a9bbd6b6ad68dbcb7d1fd8eca6e02adf13dda
Patrick Müller <patrick.e.muller@gmail.com>
2020-06-06 01:58:39 -0300

    Add docker namespace config & update TLP to powersave config

--- setup.sh
+++ setup.sh
@@ -22,9 +22,24 @@ for i in ${!FILES_TO_SOURCE[*]}; do
 done
 
 echo "Copying TLP config file ..."
-cp tlp.conf /etc/tlp.conf
+sudo cp tlp.conf /etc/tlp.conf
 
 echo "Restoring Tilix configuration ..."
 dconf load /com/gexperts/Tilix/ < tilix.dconf
+
+echo "Configuring Docker namespace files"
+sudo touch /etc/subgid
+sudo touch /etc/subuid
+echo "$LOGNAME:1000:1" | sudo tee -a /etc/subgid /etc/subuid
+echo "$LOGNAME:100000:65536" | sudo tee -a /etc/subgid /etc/subuid
+
+sudo touch /etc/docker/daemon.json
+echo "
+{
+	\"userns-remap\": \"$LOGNAME\"
+}
+" | sudo tee -a /etc/docker/daemon.json
+
+sudo systemctl restart docker
 
 echo "Done!"
