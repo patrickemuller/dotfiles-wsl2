6613c21018637356992b252052b586c055b3c222
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2020-11-05 11:09:28 -0300

    Add Ruby-build rbenv to setup.sh

--- setup.sh
+++ setup.sh
@@ -40,6 +40,17 @@ git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
 echo "Installing Powerlevel10k ZSH theme"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
+echo "Installing Rbenv and Ruby-build (always good to have, and also some packages for Rails Environment)"
+sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+exec $SHELL
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.zshrc
+exec $SHELL
+
 echo "Creating Projects folder inside Home"
 cd ~ && mkdir Projects
 
