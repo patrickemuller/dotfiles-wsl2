25f160bf827ab528c94833a86220a7f103146f7e
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2021-03-27 17:06:25 -0300

    Remove exec $SHELL from setup.sh

--- setup.sh
+++ setup.sh
@@ -46,10 +46,9 @@ cd
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(rbenv init -)"' >> ~/.zshrc
-exec $SHELL
+
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.zshrc
-exec $SHELL
 
 echo "Creating Projects folder inside Home"
 cd ~ && mkdir Projects
