9bee9adf56ab6696417626e26cc6087c1bad7d4c
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2021-08-02 11:10:35 -0300

    Updates setup.sh with a lot of modifications and improvements

--- setup.sh
+++ setup.sh
@@ -10,6 +10,21 @@ declare -r -a FILES_TO_SOURCE=(
   "p10k.zsh"
 )
 
+echo "Installing ZSH"
+sudo apt install zsh
+chsh -s $(which zsh)
+
+echo "Installing APT FAST"
+sudo add-apt-repository ppa:apt-fast/stable
+sudo apt-get update
+sudo apt-get -y install apt-fast
+
+echo "Installing aria2 to improve wget speed"
+sudo apt-get -y install aria2
+
+echo "Installing ZSH"
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # copy bash_files
 for i in ${!FILES_TO_SOURCE[*]}; do
   file="$HOME/.${FILES_TO_SOURCE[$i]}"
@@ -20,11 +35,6 @@ done
 echo "Installing Git Auto Completion"
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
-echo "Installing APT FAST"
-sudo add-apt-repository ppa:apt-fast/stable
-sudo apt-get update
-sudo apt-get -y install apt-fast
-
 echo "Installing basic ZSH plugins"
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -33,14 +43,37 @@ git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
 echo "Installing Powerlevel10k ZSH theme"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
+echo "Configuring GIT variables"
+git config --global color.ui true
+
+read -p "Enter Your Name: " username
+git config --global user.name $username
+
+read -p "Enter Your Email: " email
+git config --global user.email $email
+
+ssh-keygen -t ed25519 -C $email
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+echo "\n ======= COPY THIS SSH KEY TO YOUR GITHUB ACCOUNT ======== \n"
+cat ~/.ssh/id_ed25519.pub
+echo "\n ======= COPY THIS SSH KEY TO YOUR GITHUB ACCOUNT ======== \n"
+
 echo "Installing Rbenv and Ruby-build (always good to have, and also some packages for Rails Environment)"
-sudo apt-get -y install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
-cd
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf install ruby 3.0.2
 
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.zshrc
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-echo "Done, you may now start coding, your lazy dev!"
+install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+
+echo "Install PostgreSQL"
+install postgresql postgresql-contrib
+sudo service postgresql status
+sudo service postgresql start
+sudo passwd postgres
+
+echo "DONE!!! Close your Terminal and open it again"
